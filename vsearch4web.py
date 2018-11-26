b50babff6ffe0eda13e718036e6f2d41a28fce75
Ivan Coric <Ivan.Coric@endava.com>
2018-11-26 16:42:06 +0100

    log_regquest and view_the_log add

--- vsearch4web.py
+++ vsearch4web.py
@@ -3,12 +3,18 @@ from vsearch import search4letters
 
 app = Flask(__name__)
 
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log', 'a') as log:
+        print(req, res, file=log)
+
+
 @app.route('/search4', methods=['POST'])
 def do_search():
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = "Here are your results: "
     results = str(search4letters(phrase, letters))
+    log_request(request, results)
     return render_template('results.html',
                             the_title = title,
                             the_phrase = phrase,
@@ -20,5 +26,13 @@ def do_search():
 def entery_page():
     return render_template('entery.html',
                             the_title = "Welcome to search4letters on the web!")
+
+
+@app.route('/viewlog')
+def view_the_log() -> str:
+    with open('vsearch.log') as log:
+        contents = log.read()
+    return contents
+
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
